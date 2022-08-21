8c805d707571dabfeb30842dd1cbd4d134cc85cc
DevDaamu <107494564+DevDaamu@users.noreply.github.com>
2022-08-21 14:37:52 +0400

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,18 @@
+from math import remainder
+
+
+print("Simple Calculator")#Use of print Statment
+x = 40#Integer data type can be changed
+y = 10#Integer data type can be changed
+
+sum = x + y #Sum of x and y
+difference = x - y #Difference of x and y
+product = x * y #Product of x and y
+quotient = x / y #Quotient of x and y
+remainder = x % y #Remainder of x and y
+
+print("The sum of x and y is", sum) #Printing the sum
+print("The difference of x and y is", difference) #Printing the difference
+print("The product of x and y is", product) #Printing the product
+print("The quotient of x and y is", quotient) #Printing the quotient
+print("The remainder of x and y is", remainder) #Printing the remainder
